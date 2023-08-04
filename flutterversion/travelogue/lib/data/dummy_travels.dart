f1e35492a693dd0eb7d98dad382fa2e9acdaa5c1
Douglas <douglasfabio.ifsp@gmail.com>
2023-08-04 09:47:56 -0300

    add realm

--- flutterversion/travelogue/lib/data/dummy_travels.dart
+++ flutterversion/travelogue/lib/data/dummy_travels.dart
@@ -1,8 +1,8 @@
 import 'package:travelogue/models/travel.dart';
 
 const DUMMY_TRAVELS = {
-  '1': Travel(id: '1', name: 'Londres', travelDate: 'Data não inserida'),
-  '2': Travel(id: '2', name: 'Barcelona', travelDate: 'Data não inserida'),
-  '3': Travel(id: '3', name: 'Berlim', travelDate: 'Data não inserida'),
-  '4': Travel(id: '4', name: 'Kiev', travelDate: 'Data não inserida'),
+  '1': Travel(id: '1', name: 'Londres', travelDate: '01/06/2009'),
+  '2': Travel(id: '2', name: 'Barcelona', travelDate: '15/08/2011'),
+  '3': Travel(id: '3', name: 'Berlim', travelDate: '27/04/2020'),
+  '4': Travel(id: '4', name: 'Kiev', travelDate: '10/10/2022'),
 };
